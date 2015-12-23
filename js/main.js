a8682de37382acf9126f1dcf2e92260676d46d40
Sal Niro <salvatore.niro@outlook.com>
2015-12-23 10:21:52 -0700

    adding scripting for accessibiliyt

--- js/main.js
+++ js/main.js
@@ -16,18 +16,19 @@ function appendAbout(pane, json) {
 }
 
 function appendPresence(pane, json) {
-    for(var i = 0; i < json.length; i += 1) {
-        pane.innerHTML += '<a href="' + json[i].url + '" alt="' + json[i].path + '" target="_blank"><img src="img/' + json[i].path + '.png" />'
+    for (var i = 0; i < json.length; i += 1) {
+        pane.innerHTML += '<a href="' + json[i].url + '" target="_blank" class="presence-link"><img src="img/' + json[i].path + '.png" alt="' + json[i].path + '"/>'
     }
 }
 
 function appendSkills(pane, json) {
-    for(var i = 0; i < json.length; i += 1) {
+    for (var i = 0; i < json.length; i += 1) {
         pane.innerHTML += '<span class="skill">' + json[i] + '</span>'
     }
 }
 
 (function () {
+
     var panes = {
         about: document.getElementsByClassName('about')[0],
         presence: document.getElementsByClassName('presence')[0],
@@ -42,11 +43,42 @@ function appendSkills(pane, json) {
 
     var tabs = document.getElementsByClassName('tab-label-content');
 
-    for(var i = 0; i < tabs.length; i += 1) {
-        tabs[i].addEventListener('keydown', function(e) {
+    var mail = document.getElementsByClassName('mail')[0];
 
-            if(e.keyCode === 13) {
-                this.firstElementChild.click()
+    mail.addEventListener('keyup', function (e) {
+        if (e.keyCode === 9)
+            this.style.color = '#ffeb3b';
+    });
+
+    mail.addEventListener('blur', function () {
+        this.style.color = '#525252';
+    });
+
+    for (var i = 0; i < tabs.length; i += 1) {
+
+        tabs[i].addEventListener('blur', function () {
+            for (var i = 0; i < tabs.length; i += 1)
+                tabs[i].firstElementChild.style.border = 0
+        });
+
+        tabs[i].addEventListener('click', function () {
+            for (var i = 0; i < tabs.length; i += 1) {
+                tabs[i].firstElementChild.style.border = 0
+                tabs[i].setAttribute('aria-selected', 'false');
+            }
+
+            this.setAttribute('aria-selected', 'true')
+        });
+
+        tabs[i].addEventListener('keyup', function (e) {
+            if (e.keyCode === 13 && e.target.classList.contains('tab-label-content'))
+                this.firstElementChild.click();
+
+            if (e.keyCode === 9) {
+                for (var o = 0; o < tabs.length; o += 1)
+                    tabs[o].firstElementChild.style.border = 0
+
+                this.firstElementChild.style.border = e.target.classList.contains('presence-link') ? 0 : 'dotted 1px white'
             }
         });
     }
